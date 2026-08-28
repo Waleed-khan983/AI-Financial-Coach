f6b560ee16a48ab39afbcf7bc0c952d1e6e5e955
waleed khan <waleedkhansafimd983@gmail.com>
2026-08-28 10:16:08 +0500

    update and delete Transactions contollers and routes are added

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -134,3 +134,104 @@ export const getTransactionById = async (req, res, next) => {
         next(err)
     }
 }
+
+
+// update Transaction
+
+export const updateTransaction = async (req, res, next) => {
+    try{
+
+        const { id } = req.params;
+
+        const {
+            transactionType,
+            transactionAmount,
+            transactionCategory,
+            description,
+            transactionDate,
+            paymentMethod,
+            isRecurring,
+            recurringFrequency,
+            transactionNotes,
+
+        }
+            = req.body;
+
+        
+
+        const transaction = await Transaction.findOne(
+            {
+                _id: id,
+                userId: req.user._id,
+            }
+        );
+
+        if (!transaction) {
+            return res.status(400).json({
+                sucess: false,
+                message: "Transaction not found",
+                status: 400,
+            })
+        }
+
+
+      const updatedTransactions =  await Transaction.findByIdAndUpdate(id, {
+            transactionType,
+            transactionAmount,
+            transactionCategory,
+            description,
+            transactionDate,
+            paymentMethod,
+            isRecurring,
+            recurringFrequency,
+            transactionNotes,
+        },
+        {
+            new: true,
+            runValidators: true,
+        }
+    )
+
+        return res.status(200).json({
+            success: true,
+            message: "Transaction updated successfully",
+            transaction: updatedTransactions,
+            status: 200,
+        });
+
+        
+
+    }catch(err){
+        next(err)
+    }
+}
+
+
+// delete Transaction
+export const deleteTransaction = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+
+        const transaction = await Transaction.findOneAndDelete({
+            _id: id,
+            userId: req.user._id,
+        });
+
+        if (!transaction) {
+            return res.status(404).json({
+                success: false,
+                message: "Transaction not found",
+                status: 404,
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Transaction deleted successfully",
+            status: 200,
+        });
+
+    } catch (err) {
+        next(err);
+    }
+};
--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -3,6 +3,8 @@ import {
     createTransaction,
     getALLTransactions,
     getTransactionById,
+    updateTransaction,
+    deleteTransaction,
 
 } from "../controllers/transactionController.js";
 import { authMiddleware } from "../middlewars/authMiddleware.js";
@@ -12,5 +14,7 @@ const router = express.Router();
 router.post("/create", authMiddleware, createTransaction);
 router.get("/all", authMiddleware, getALLTransactions);
 router.get("/:id", authMiddleware, getTransactionById);
+router.put("/update/:id", authMiddleware, updateTransaction);
+router.delete("/delete/:id", authMiddleware, deleteTransaction);
 
 export default router;
